9c9d2ad5fdd145a792bb26e517250be6354b085d
s4lfanet <wardian370@gmail.com>
2026-09-02 22:06:11 +0700

    chore: remove temp cs test scripts

--- scripts/tmp_delete_cs_test.js
+++
@@ -1,9 +0,0 @@
-// Temporary script: delete the CUSTOMER_SERVICE test user created by tmp_create_cs_test.js
-const { PrismaClient } = require('@prisma/client');
-
-(async () => {
-  const prisma = new PrismaClient();
-  const res = await prisma.adminUser.deleteMany({ where: { username: 'cstest' } });
-  console.log('Deleted:', res.count);
-  await prisma.$disconnect();
-})();
