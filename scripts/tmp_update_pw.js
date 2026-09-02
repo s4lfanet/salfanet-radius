6e2006c18d738d255ad585dcd416663bb767b0f5
s4lfanet <wardian370@gmail.com>
2026-09-02 08:29:29 +0700

    tmp: use prisma for pw update

--- scripts/tmp_update_pw.js
+++ scripts/tmp_update_pw.js
@@ -1,13 +1,11 @@
-const mysql = require('mysql2/promise');
+const { PrismaClient } = require('@prisma/client');
 (async () => {
-  const conn = await mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'salfanet_user',
-    password: '1c0921f1e4871379f29727e404a3d27b95f9081d23f0eeac',
-    database: 'salfanet_radius'
-  });
+  const prisma = new PrismaClient();
   const hash = '$2b$10$8O3NbqYypjI5icdI.FTEYedMmwexNqfvqyEy5OUGE7cjDkHj/U9T.';
-  await conn.execute("UPDATE admin_users SET password=? WHERE username=?", [hash, 'superadmin']);
-  console.log('Password updated');
-  await conn.end();
+  const result = await prisma.adminUser.updateMany({
+    where: { username: 'superadmin' },
+    data: { password: hash }
+  });
+  console.log('Updated rows:', result.count);
+  await prisma.$disconnect();
 })();
